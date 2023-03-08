988d635f3211e437dfe9f86a9f3aecec1f21c737
rudy66-git <59192629+rudy66-git@users.noreply.github.com>
2023-03-08 15:40:56 +0530

    Add files via upload

---
+++ JMail/WEB-INF/src/MarkAllAsRead.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MarkAllAsRead extends HttpServlet{
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    MailReader reader = new MailReader();
+    reader.markAllAsRead();
+
+  }
+}
